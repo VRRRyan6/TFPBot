502a0ce15ca07d0b25abf0862454c185cd622bd1
DrPepper <josephcrain14@gmail.com>
2023-02-27 22:21:17 -0500

    have status change every 3 minutes

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,10 @@
 import fs = require('node:fs');
 
-export function getJsFiles(path: string) {
+/**
+ * Read a specified directory and grab compiled javascript files
+ * @param path Exact path of directory to read
+ */
+export function getJsFiles(path: string): string[] {
     return fs.readdirSync(path).filter((file) => file.endsWith('.js'));
 }
 
--- src/util/randomStatus.ts
+++ src/util/randomStatus.ts
@@ -1,20 +1,32 @@
 import { ActivityOptions, ActivityType, Client, Events } from 'discord.js';
-import { yellow } from 'ansi-colors';
+import { green, yellow } from 'ansi-colors';
 
 const statusMessages: ActivityOptions[] = [
     {
         name: 'over the peasants',
         type: ActivityType.Watching
+    },
+    {
+        name: 'with your feelings',
+        type: ActivityType.Playing
     }
-]
+];
 
 module.exports = {
     name: 'randomStatus',
     event: Events.ClientReady,
     execute(client: Client) {
-        const status = statusMessages[Math.floor(statusMessages.length * Math.random())]
-        if (!status) return console.log(yellow(`[randomStatus] Missing statusMessages, not running utility.`))
+        const initialStatus = statusMessages[Math.floor(statusMessages.length * Math.random())];
+        if (!initialStatus) return console.log(yellow(`[randomStatus] Missing statusMessages, not running utility.`));
 
-        return client.user?.setActivity(status)
+        // Change status every 3 minutes
+        setInterval(() => {
+            // Remember to grab a new status from the array, if we use the var above it'll always be the same.
+            const status = statusMessages[Math.floor(statusMessages.length * Math.random())]
+            client.user?.setActivity(status)
+        }, (3 * 60 * 1000));
+
+        console.log(green(`[randomStatus] Initialized function selected "${initialStatus.name}" as initial status.`))
+        return client.user?.setActivity(initialStatus);
     }
 }
