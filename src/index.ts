e0a5428e26867006ff2aaa79cffddaf9dbbe973e
DrPepperG <josephcrain14@gmail.com>
2023-09-23 22:36:00 -0400

    1.0.2

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { config as dotenv } from 'dotenv';
 dotenv();
 
 // This is automatically updated when npm version is ran successfully
-process.env.version = '1.0.1';
+process.env.version = '1.0.2';
 
 // Default imports
 import { getJsFiles } from './helpers.js';
