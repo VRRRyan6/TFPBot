549dde442318bc4ddbc0b6bb1256d6d31048c38c
DrPepperG <josephcrain14@gmail.com>
2023-09-23 22:35:49 -0400

    improve timings on command handling

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,37 +1,56 @@
-import { Events, type BaseInteraction } from 'discord.js'
+import { 
+    Events,
+    InteractionType,
+    type AutocompleteInteraction,
+    type BaseInteraction,
+    type ChatInputCommandInteraction,
+} from 'discord.js'
+import { Command } from '../typings/index.js';
 
 export default {
     name: Events.InteractionCreate,
     async execute(interaction: BaseInteraction) {
-        if (interaction.isChatInputCommand()) {
-            const command = interaction.client.commands.get(interaction.commandName);
+        switch(interaction.type) {
+            case InteractionType.ApplicationCommand:
+                if (!interaction.isChatInputCommand()) return;
 
-            if (!command) {
-                console.error(`No command matching ${interaction.commandName} was found.`);
-                return;
-            }
+                handleCommand(interaction, (command, interaction) => {
+                    if (!interaction.isChatInputCommand()) return;
+                    command.execute(interaction);
+                });
 
-            try {
-                await command.execute(interaction);
-            } catch (error) {
-                console.error(`Error executing ${interaction.commandName}`);
-                console.error(error);
-            }
-        } else if (interaction.isAutocomplete()) {
-            const command = interaction.client.commands.get(interaction.commandName);
+                break;
+            case InteractionType.ApplicationCommandAutocomplete:
+                if (!interaction.isAutocomplete()) return;
+                
+                handleCommand(interaction, (command, interaction) => {
+                    if (!interaction.isAutocomplete() || !command.autocomplete) return;
+                    command.autocomplete(interaction);
+                })
 
-            if (!command) {
-                console.error(`No command matching ${interaction.commandName} was found.`);
-                return;
-            }
-
-            try {
-                if (!command.autocomplete) return;
-                await command.autocomplete(interaction);
-            } catch (error) {
-                console.error(`Error executing ${interaction.commandName}`);
-                console.error(error);
-            }
+                break;
         }
     },
 };
+
+async function handleCommand(
+    interaction: ChatInputCommandInteraction | AutocompleteInteraction,
+    callback: (
+        command: Command,
+        interaction: ChatInputCommandInteraction | AutocompleteInteraction) => void
+    ) {
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        callback(command, interaction);
+    } catch (error) {
+        console.error(`Error executing ${interaction.commandName}`);
+        console.error(error);
+    }
+}
